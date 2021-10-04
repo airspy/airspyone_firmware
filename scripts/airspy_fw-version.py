21ce1f557529091e62c565e3dd1b1ad97256c31b
bvernoux <bvernoux@gmail.com>
2021-10-04 22:13:34 +0200

    Fixed issue https://github.com/airspy/airspyone_firmware/issues/22 to build airspy firmware with Windows, Debian/Ubuntu Linux because of deprecated Python dependencies (mainly pip YAML) libopencm3 added nvic header automatically generated by scripts/irq2nvic_h to remove dependency on any Python script libopencm3 removed any dependencies with python scripts

--- scripts/airspy_fw-version.py
+++ scripts/airspy_fw-version.py
@@ -12,8 +12,8 @@ parser = OptionParser(usage=usage)
 if len(args)==1:
   sys.stdout = open(args[0], 'w')
   git=Repo(search_parent_directories=True).git
-  print '#define AIRSPY_FW_GIT_TAG "' + git.describe(tags=True,always=True,dirty=True,long=True) + '"'
-  print '#define AIRSPY_FW_CHECKIN_DATE "' + git.show(['--pretty=format:%ai']).partition(' ')[0] + '"'
+  print('#define AIRSPY_FW_GIT_TAG "' + git.describe(tags=True,always=True,dirty=True,long=True) + '"')
+  print('#define AIRSPY_FW_CHECKIN_DATE "' + git.show(['-s', '--pretty=format:%ai']).partition(' ')[0] + '"')
 else:
   parser.print_help()
   sys.exit(1)
